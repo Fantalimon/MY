bb27c3a805b05a4433e856b0ee29d0f4fac85315
Dimka <manager@perilman.com.ua>
2017-03-10 23:52:35 +0200

    1 task do

--- DZ7/hello.php
+++ DZ7/hello.php
@@ -1,1 +1,17 @@
 <?php
+$username=$_COOKIE['username']
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hello</title>
+</head>
+<body>
+<h3><a href="index.php">Переход на страничку inex</a></h3>
+<h4> Привет  <?php echo $username ?> </h4>
+
+</body>
+</html>
+<?php
+?>
--- DZ7/index.php
+++ DZ7/index.php
@@ -1,1 +1,32 @@
 <?php
+$username='';
+if (!empty($_GET)) {
+    $username = empty($_GET['username']) ? '' : trim(strip_tags($_GET['username']));
+}
+setcookie('username',$username,time()+3600,"DZ7");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+</head>
+<body>
+<h3><a href="hello.php">Переход на страничку hello</a></h3>
+
+<form ​ action ​ = "index.php" ​ method ​ =" ​ GET ​ ">
+    <fieldset title="Ваше имя">
+        <legend>Введите имя</legend>
+        <input ​type="text" ​ name="username">
+        <br>
+        <br>
+        <input type="submit" value="Отправить ваш вариант">
+    </fieldset>
+</form>
+
+
+</body>
+</html>
+<?php
+?>
