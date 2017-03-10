3910d37602966f3e6e489779f3c6b740f788eaa5
Dimka <manager@perilman.com.ua>
2017-03-11 00:46:46 +0200

    3 task do

--- DZ7/index.php
+++ DZ7/index.php
@@ -1,12 +1,14 @@
 <?php
 $username='';
 $phone='';
+$age=mt_rand(10,70);
 if (!empty($_GET)) {
     $username = empty($_GET['username']) ? '' : trim(strip_tags($_GET['username']));
     $phone = empty($_GET['phone']) ? '' : trim(strip_tags($_GET['phone']));
 }
 setcookie('username',$username,time()+3600,"DZ7");
 setcookie('phone',$phone,time()+3600,"DZ7");
+setcookie('age',$age,time()+3600*3,"DZ7");
 ?>
 <!DOCTYPE html>
 <html lang="en">
