057e9d01f6924905d01c57c2d909d16b7659dbfe
Dimka <manager@perilman.com.ua>
2017-03-12 14:10:21 +0200

    5 index 1/2  worck 2

--- DZ7/index.php
+++ DZ7/index.php
@@ -12,7 +12,7 @@ if (!empty($_GET)) {
 setcookie('username', $username, time() + 3600, "DZ7");
 setcookie('phone', $phone, time() + 3600, "DZ7");
 setcookie('age', $age, time() + 3600 * 3, "DZ7");
-setcookie('bertnday', $berthday, time() + 31536000.00042889);
+setcookie('bertnday', $berthday, time() + 31536000.00042889, "DZ7");
 
 ?>
 <!DOCTYPE html>
@@ -45,19 +45,21 @@ setcookie('bertnday', $berthday, time() + 31536000.00042889);
 </html>
 
 <?php
-if (isset($berthday)) {
+
+if (empty($_COOKIE['berthday'])) {
+    
     $berthday = strtotime($berthday);
     $difference = time() - $berthday;
     $year = ($difference % 31536000.00042889);
     $rez = (31536000.00042889 - $year);
     $rezult_day = floor($rez / 86400);
     
-    
+}
     if ($rezult_day == 364) {
         echo "<h1 style='color:red'>Поздравляем У вас сегодня день варенья!</h1>";
     } else {
         echo "Вам до дня рождения осталось, $rezult_day дней.";
     }
     
-}
+
 ?>
