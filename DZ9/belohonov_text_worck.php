3c28b79e06971363e5f96738c7a38590e2731e92
Dimka <manager@perilman.com.ua>
2017-03-23 12:15:39 +0200

    1 task do

--- DZ9/belohonov_text_worck.php
+++ DZ9/belohonov_text_worck.php
@@ -20,4 +20,57 @@ function add_number_in_file($a=0)
     }
     fclose($file);
 }
-add_number_in_file(4);
+add_number_in_file(1);
+
+/*2. Даны два файла ​ ХХХ​ и ​ YYY.​ В файлах записаны тестовые слова (на латинице,
+придумайте сами). разделенными пробелами. Необходимо написать функцию,
+которая создаст новые три файла:
+only_first.txt​ : содержащий строки, которые встречаются только в первом файле;
+both.txt​: только строки, которые встречаются в обоих файлах;
+more_two.txt​ : только строки, которые встречаются в каждом файле более двух раз.*/
+
+$xxx=fopen("xxx.txt", "r");
+$yyy=fopen("yyy.txt", "r");
+
+
+function separate_file_content($xxx,$yyy)
+{
+
+    $str_xxx=fgetss($xxx);
+    $str_yyy=fgetss($yyy);
+    $words_xxx=explode(" ",$str_xxx);
+    $words_yyy=explode(" ",$str_yyy);
+
+    $only_first=fopen("only_first.txt", "a");
+    $both=fopen("both.txt", "a");
+    $more_two=fopen("more_two.txt", "a");
+    
+    $arr=[];
+    foreach ($words_xxx as $value_x)
+    {
+       foreach ($arr as $var_x)
+       {
+           if ($var_x==$value_x) {
+               if (isset($arr[$value_x])) {
+                   $arr[$value_x]++;
+               }else{$arr[$value_x];}
+           }
+       }
+    }
+//    foreach ($arr as $rez)
+//    {
+//        fwrite($only_first, " ".$rez." ".PHP_EOL);
+//    }
+    echo "<pre>";
+        var_dump( $arr );
+    echo "</pre>";
+    
+    fclose($only_first);
+fclose($both);
+fclose($more_two);
+}
+
+separate_file_content($xxx, $yyy);
+
+fclose($xxx);
+fclose($yyy);
