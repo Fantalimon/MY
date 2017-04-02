a6b1729d82db273b14dc92b49a9b99603971bed4
Dimka <manager@perilman.com.ua>
2017-04-02 17:21:15 +0300

    button start and button home FIXED

--- OUER_SITE/destroy.php
+++ OUER_SITE/destroy.php
@@ -13,5 +13,11 @@ setcookie('question','');
 ?>
 
 <p>Your result is <?php echo  $result ?> from <?php echo $question; ?></p>
-<p><a href="index_quiz.php">Start the test again</a></p>
-<p><a href="index.php">На главную</a></p>
+
+<form action="index_quiz.php" method="post">
+    <input type="submit" value="Начать тест" />
+</form>
+<br>
+<form action="index.php" method="post" >
+<input type="submit" value="На главную" />
+</form>
--- OUER_SITE/start.php
+++ OUER_SITE/start.php
@@ -3,7 +3,7 @@
 <div style="position: absolute; left:43%; top: 45%;">
 <form action="index_quiz.php" method="post">
     <input type="hidden" name="question" value="<?php  echo ++$question; ?>">
-    <input type="submit" value="Начать тест">
+    <input type="submit" value=" Начём ">
 </form>
 </div>
 
