de579a39e9663724ba35902c522d382a8583e835
Dimka <manager@perilman.com.ua>
2017-04-04 13:02:41 +0300

    withaut in header sign

--- OUER_SITE/header.php
+++ OUER_SITE/header.php
@@ -3,8 +3,4 @@
 
 <header>
 <a href="index.php"><h1>My Site</h1></a>
-<div style="text-align: right">
-    <a href="signe_in.php">Sign in</a>
-    <a href="sineup.php">Sing up</a>
-</div>
 </header>
