c6559130e14665e9e50f98935a7af2c7a84bfbb5
Dimka <manager@perilman.com.ua>
2017-04-06 00:01:42 +0300

    withaut  directoryDZ8

--- DZ8/quiz/start.php
+++
@@ -1,7 +0,0 @@
-<h1 style="position: absolute; left: 27%; top: 20px;">Давайте пройдем тест</h1>
-<div style="position: absolute; left:43%; top: 45%;">
-<form action="index.php" method="post">
-    <input type="hidden" name="question" value="<?php echo ++$question; ?>">
-    <input type="submit" value="Начать тест">
-</form>
-</div>
