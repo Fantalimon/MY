11ff659b4def640c2213d26ab7911170a8fc09ff
Dimka <manager@perilman.com.ua>
2017-04-06 17:14:07 +0300

    rewrite site-files from quiz_session

--- OUER_SITE/footer.php
+++
@@ -1,6 +0,0 @@
-
-<footer>
-    <div>my-site.com</div>
-    <div>Вы посетили наш сайт <?php echo $visitor_count?></div>
-    <div>Последний раз вы были <?php echo $last_visit?></div>
-</footer>
--- OUER_SITE/start.php
+++
@@ -1,9 +0,0 @@
-
-<h1 style="position: absolute; left: 27%; top: 20px;">Давайте пройдем тест</h1>
-<div style="position: absolute; left:43%; top: 45%;">
-<form action="index_quiz.php" method="post">
-    <input type="hidden" name="question" value="<?php  echo ++$question; ?>">
-    <input type="submit" value=" Начём ">
-</form>
-</div>
-
