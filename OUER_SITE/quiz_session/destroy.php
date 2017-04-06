1ce298ecf0d0b0e67df937abf550062d44a7c436
Dimka <manager@perilman.com.ua>
2017-04-06 16:28:22 +0300

    add include and connect changes

--- OUER_SITE/quiz_session/destroy.php
+++ OUER_SITE/quiz_session/destroy.php
@@ -5,7 +5,7 @@
 <form action="index_quiz.php" method="get">
     <input type="submit" value="Начать тест заново">
 </form>
-    <form action="http://127.0.0.1/sit.my/MY_DZ/OUER_SITE/" method="get">
+    <form  action="http://127.0.0.1/sit.my/MY_DZ/OUER_SITE/" method="get">
     <input type="submit" value="На главную">
     </form>
 </div>
