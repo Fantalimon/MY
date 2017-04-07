360629538427eb0f535bcea75664a0e3f0501961
Dimka <manager@perilman.com.ua>
2017-04-07 22:07:22 +0300

    rewrite  forms

--- OUER_SITE/config.php
+++ OUER_SITE/config.php
@@ -11,3 +11,44 @@ define('DB_USER', $configDb['user']);
 define('DB_PASSWORD', $configDb['password']);
 define('DB_NAME', $configDb['dbname']);
 define('DB_PORT', $configDb['port']);
+
+
+function getConnection($host=DB_HOST,$username=DB_USER,$password=DB_PASSWORD,$db_name=DB_NAME,$port=DB_PORT)
+{
+    static $link=null;
+    if($link === null)
+    {
+        $link=mysqli_connect($host,$username,$password,$db_name,$port);
+        if(!$link) die('Нет соединения с базой'.mysqli_connect_error());
+        mysqli_query($link, 'SET NAMES utf8');
+    }
+    return $link;
+}
+
+function closeConnection(mysqli $link){mysqli_close($link);}
+
+
+function addUser($username,$email,$password,$created_at)
+{
+    $link=getConnection();
+    $query='insert into users (`username`,`email`,`password`,`created_at`) VALUES (?,?,?,?)';
+    $stmt = mysqli_prepare($link, $query);
+    mysqli_stmt_bind_param($stmt, 'ssss', $username,$email,$password,$created_at);
+    $rezult=mysqli_stmt_execute($stmt);
+    if(!$rezult){die('Error: '.mysqli_stmt_error($stmt));}
+    $userId=mysqli_stmt_insert_id($stmt);
+    mysqli_stmt_close($stmt);
+    return $userId;
+}
+
+
+function getUsersByEmail($email)
+{
+    $link = getConnection();
+    $email= mysqli_real_escape_string($link, $email);
+    $query="select * from users WHERE email = '$email'";
+    $rezult = mysqli_query($link, $query);
+    if(!$rezult) die('user is not exist'.mysqli_error($link));
+    
+    return mysqli_fetch_assoc($rezult);
+}
--- OUER_SITE/signe_up_hendler_mysql.php
+++ OUER_SITE/signe_up_hendler_mysql.php
@@ -85,6 +85,36 @@ else {
 }
 else{header(LOCATION);}
 */
+include "config.php";
 
-
+if (!empty($_POST)) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $created_at=date("Y-m-d H:i:s");
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    
+    if (empty(getUsersByEmail($email))) {
+        
+        $userId = addUser($username,$email,$password,$created_at);
+        if(empty($userId)){die('User has not been added!');}
+        
+        $password=md5($password);
+        
+        $user = [
+            'username' => $username,
+            'email' => $email,
+            'password' => $password,
+            'created_at'=> $created_at,
+            'user_id' => $userId
+        ];
+        
+        
+        
+    }
+    
+    $_SESSION['userdata'] = serialize($user);
+    header('location: '.SITE);
+    
+}
 ?>
