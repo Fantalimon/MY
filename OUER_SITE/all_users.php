a4c0c9062010b04dc9a919f5a381793da0c47daf
Dimka <manager@perilman.com.ua>
2017-04-07 20:45:24 +0300

    addet hrefs edit and deleted

--- OUER_SITE/all_users.php
+++ OUER_SITE/all_users.php
@@ -17,21 +17,28 @@ function closeConnection(mysqli $link){mysqli_close($link);}
 
 $row=[];
 
+$edit="<a href='edit_user.php'>edit</a>";
+$delete="<a href='delete_user.php'>delete</a>";
+
+
 $link=getConnection();
 
 $query='SELECT * FROM users';
 
 $result=mysqli_query($link,$query);
-echo "<div style='position: relative;left: 25%; ;text-align: center;'>";
-echo "<table border='1'; style='border: solid; width: 50%'>";
+
+echo "<div style='position: relative;left: 10%; ;text-align: center;'>";
+echo "<table border='1' style='border: solid; width: 75%'>";
 
 echo "<thead>";
 echo "<tr>";
-echo "<th>".'id'."</th>";
-echo "<th>".'username'."</th>";
-echo "<th>".'email'."</th>";
+  echo "<th>".'id'."</th>";
+  echo "<th>".'username'."</th>";
+  echo "<th>".'email'."</th>";
+  echo "<th>".'action'."</th>";
 echo "</tr>";
 echo "</thead>";
+
 while($row=mysqli_fetch_array($result))
 {
     $id=$row['user_id'];
@@ -44,10 +51,14 @@ echo "<tbody >";
     echo "<td>".$name."</td>";
   
     echo "<td>".$email."</td>";
+    
+    echo "<td>"."$edit "." $delete"."</td>";
+    
   echo "</tr>";
 echo "</tbody>";
 }
 
 echo "</table>";
 echo "</div>";
+
 closeConnection($link);
