cfa902db3adbc0e51dd4ae58c7cb38f664fa1170
Dimka <manager@perilman.com.ua>
2017-04-07 21:34:02 +0300

    add form

--- OUER_SITE/edit_user.php
+++ OUER_SITE/edit_user.php
@@ -1,1 +1,31 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>form</title>
+</head>
+<body>
+<form>
+    <fieldset title="персональные данные">
+        <legend>Ваши данные</legend>
+        <br>
+        <input type="text" title="имя" name="username" placeholder="имя" />
+        <br>
+        <br>
+        <input type="text" title="почта" name="email" placeholder="ваша почта" />
+        <br>
+        <br>
+        <input type="text" title="отчество" name="password" placeholder="пароль" />
+        <br>
+        <br>
+        <input type="submit" name="sub_edit" value="отправить новые данные" />
+    </fieldset>
+</form>
+
+</body>
+</html>
+
+
