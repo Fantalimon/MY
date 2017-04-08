a25c194d26a250fa13bd764bba070550ab8c1248
Dimka <manager@perilman.com.ua>
2017-04-08 11:01:04 +0300

    changes

---
+++ OUER_SITE/edit.php
@@ -0,0 +1,19 @@
+<?php
+include "config.php";
+if(!empty($_POST)){
+   $username =  trim(strip_tags($_POST['username']));
+   $email = trim(strip_tags($_POST['email']));
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    $password =  trim(strip_tags($_POST['password']));
+    $created_at=date("Y-m-d H:i:s");
+}
+
+$link=getConnection();
+
+readdUser($username, $email, $password, $created_at);
+
+
+
+
+
+closeConnection($link);
