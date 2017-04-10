2625cc812977e90525fb7f681ecdcb99e7598456
Dimka <manager@perilman.com.ua>
2017-04-11 01:11:41 +0300

    tasck 3 do

--- oop.php
+++ oop.php
@@ -1,13 +1,26 @@
 <?php
 class User
 {
-    public $name;
+    public $firstname;
     public $lastname;
     public $email;
     public $phone;
     
-    public function setName($name){$this->name=$name;}
-    public function getName(){echo $this->name;}
+    public function __construct($firstname,$lastname,$email,$phone)
+    {
+        $this->setFirstname($firstname);
+        $this->setLastname($lastname);
+        $this->setEmail($email);
+        $this->setPhone($phone);
+        
+        $this->getFirstname();
+        $this->getLastname();
+        $this->getEmail();
+        $this->getPhone();
+    }
+    
+    public function setFirstname($firstname){$this->firstname=$firstname;}
+    public function getFirstname(){echo $this->firstname;}
    
     public function setLastname($lastname){$this->lastname=$lastname;}
     public function getLastname(){echo $this->lastname;}
@@ -19,9 +32,11 @@ class User
     public function getPhone(){$this->phone;}
 }
 
-$newUser=new User();
-$newUser->setName('Дима');
-$newUser->getName();
+//$newUser=new User();
+//$newUser->setFirstname('Дима');
+//$newUser->getFirstname();
 
+
+$userTwo= new User($firstname="Денис", $lastname="Попов", $email="mail@mail.com", $phone="123432145");
 
 
