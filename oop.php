aab5e3331a0bcb6622330385a7772f1d5cfd6ea3
Dimka <manager@perilman.com.ua>
2017-04-11 00:48:05 +0300

    one tasck dogit st!

--- oop.php
+++ oop.php
@@ -1,1 +1,11 @@
 <?php
+class User
+{
+    public $name;
+    public function setName($name){$this->name=$name;}
+    public function getName(){echo $this->name;}
+}
+
+$newUser=new User();
+$newUser->setName('Дима');
+$newUser->getName();
