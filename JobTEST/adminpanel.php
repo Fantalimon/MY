430831e422701254d289a500de30bfe6cfa5f438
Dimka <manager@perilman.com.ua>
2017-05-12 00:52:44 +0300

    select

--- JobTEST/adminpanel.php
+++ JobTEST/adminpanel.php
@@ -1,16 +1,42 @@
 <?php
 include_once 'conf.php';
 
-function show ()
-{
+    $row='';
     $link=getConnection();
-    $query="SELECT `username`,`email`,`created_at`,`text` FROM userstext";
-    $rezult=mysqli_query($link, $query);
- 
+    $query="SELECT `username`,`email`,`created_at`,`text` FROM userstext ";
+
+//$_GET['status']='up';
+//$_GET['colon']='username';
+$colon=$_GET['colon'];
+($_GET['status']=='up' )?$query.="ORDER BY $colon ASC":$query.="ORDER BY $colon DESC";
+
+$rezult=mysqli_query($link, $query);
 
 echo "<div style='position: relative;left: 10%; ;text-align: center;'>";
 echo "<table border='1' style='border: solid; width: 75%'>";
 echo "<thead>";
+
+echo "<tr>";
+
+echo "<td>"."<a href='adminpanel.php?status=up&colon=username'>up</a>";
+echo " / ";
+echo "<a href='adminpanel.php?status=doun&colon=username'>doun</a>"."</td>";
+
+echo "<td>"."<a href='adminpanel.php?status=up&colon=email'>up</a>";
+echo " / ";
+echo "<a href='adminpanel.php?status=doun&colon=email'>doun</a>"."</td>";
+
+echo "<td>"."<a href='adminpanel.php?status=up&colon=created_at'>up</a>";
+echo " / ";
+echo "<a href='adminpanel.php?status=doun&colon=created_at'>doun</a>"."</td>";
+
+echo "<td>"."<a href='adminpanel.php?status=up&colon=text'>up</a>";
+echo " / ";
+echo "<a href='adminpanel.php?status=doun&colon=text'>doun</a>"."</td>";
+
+
+echo "</tr>";
+
 echo "<tr>";
 echo "<th>".'username'."</th>";
 echo "<th>".'email'."</th>";
@@ -27,24 +53,13 @@ while($row = mysqli_fetch_array($rezult, MYSQLI_ASSOC))
     $created_at = $row['created_at'];
     $text = $row['text'];
     
-    /*
-    $edit = "<a href='edit_user.php?user_id=$user_id&username=$username&email=$email&password=$password'>"."edit"."</a> ";
-    $delete = "<a href='delete_user.php?user_id=$user_id ' onclick='confirm(Действительно удалить?)'>";
-    $delete.="delete";
-    $delete.="</a> ";*/
-    
     echo "<tbody >";
     echo "<tr>";
     
-    
     echo "<td>" . $username . "</td>";
-    
-    echo "<td>" .  $email . "</td>";
-    
+    echo "<td>" . $email . "</td>";
     echo "<td>" . $created_at. "</td>";
     echo "<td>" . $text. "</td>";
-    
-    echo "</td>";
     
     echo "</tr>";
     echo "</tbody>";
@@ -56,4 +71,3 @@ echo "</div>";
     
     if(!$rezult){if(!$rezult) die('ERROR'.mysqli_error($link));}
     mysqli_close($link);
-}
