a8eeeb3eb27cd10ab5f3292cfe06c60799c2dc5e
Dimka <manager@perilman.com.ua>
2017-05-11 14:35:21 +0300

    add in table parameters

--- JobTEST/conf.php
+++ JobTEST/conf.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'posteg.php';
 
 $config=parse_ini_file('config.ini',true);
 
@@ -30,12 +31,12 @@ function getConnection($host=DB_HOST,$username=DB_USER,$password=DB_PASSWORD,$db
 function closeConnection(mysqli $link){mysqli_close($link);}
 
 
-function addUser($username,$email,$password,$created_at)
+function addmassege($username,$email,$hompage,$ip,$brouser,$created_at)
 {
     $link=getConnection();
-    $query='insert into users (`username`,`email`,`password`,`created_at`) VALUES (?,?,?,?)';
+    $query='insert into  (`username`,`email`,`hompage`,`ip`,`brouser`,`created_at`) VALUES (?,?,?,?,?,?)';
     $stmt = mysqli_prepare($link, $query);
-    mysqli_stmt_bind_param($stmt, 'ssss', $username,$email,$password,$created_at);
+    mysqli_stmt_bind_param($stmt, 'ssssss', $username,$email,$hompage,$ip,$brouser,$created_at);
     $rezult=mysqli_stmt_execute($stmt);
     if(!$rezult){die('Error: '.mysqli_stmt_error($stmt));}
     $userId=mysqli_stmt_insert_id($stmt);
@@ -43,49 +44,3 @@ function addUser($username,$email,$password,$created_at)
     return $userId;
 }
 
-function readdUser($username,$email,$password,$created_at,$user_id)
-{
-    $link=getConnection();
-    $query="update users set username= '$username', email='$email', password='$password', created_at='$created_at' WHERE user_id='$user_id'";
-    $rezult = mysqli_query($link, $query);
-    if(!$rezult){die('error : '.mysqli_error($link));}
-    closeConnection($link);
-    return $rezult;
-}
-
-
-function delUser($user_id)
-{
-    
-    $link=getConnection();
-    $query="delete from users WHERE user_id='$user_id'";
-    $rezult = mysqli_query($link, $query);
-    if(!$rezult){die('error : '.mysqli_error($link));}
-    closeConnection($link);
-    return $rezult;
-}
-
-
-
-function getUsersByEmail($email)
-{
-    $link = getConnection();
-    $email= mysqli_real_escape_string($link, $email);
-    $query="select * from users WHERE email = '$email'";
-    $rezult = mysqli_query($link, $query);
-    if(!$rezult) die('user is not exist'.mysqli_error($link));
-    
-    return mysqli_fetch_assoc($rezult);
-}
-
-
-
-function signeInUser ($username_in,$email_in,$password_in)
-{
-    $link = getConnection();
-    $query="SELECT * FROM users where user_id and username like '$username_in' and email LIKE  '$email_in' AND password like '$password_in'";
-    $rezult = mysqli_query($link, $query);
-    if(!$rezult){die('error : '.mysqli_error($link));}
-    return mysqli_fetch_assoc($rezult);
-    closeConnection($link);
-}
