4daf16fbc456459d57cad87de0a71470ac5b9b7b
Dimka <manager@perilman.com.ua>
2017-09-08 01:44:38 +0300

    можно отправлять форму когда заполненны поля имя территория и почта

--- Artjoker/add.php
+++ Artjoker/add.php
@@ -1,37 +1,39 @@
 <?php
 include_once 'autoload.php';
 
-$name='';
-$email='';
-$Territory='';
-$RayonsTowns='';
-$Rayons='';
-$Towns='';
-$response=[];
+$name = '';
+$email = '';
+$Territory = '';
+$RayonsTowns = '';
+$Rayons = '';
+$Towns = '';
+$SMT='';
+$response = [];
+
+$name = isset($_POST['name']) ? htmlspecialchars(strip_tags(trim($_POST['name']))) : '';
+$email = isset($_POST['email']) ? htmlspecialchars(strip_tags(trim($_POST['email']))) : '';
+$Territory = isset($_POST['Territory']) ? htmlspecialchars(strip_tags(trim($_POST['Territory']))) : '';
+$Towns = isset($_POST['Towns']) ? htmlspecialchars(strip_tags(trim($_POST['Towns']))) : '';
+$RayonsTowns = isset($_POST['RayonsTowns']) ? htmlspecialchars(strip_tags(trim($_POST['RayonsTowns']))) : '';
+$Rayons = isset($_POST['Rayons']) ? htmlspecialchars(strip_tags(trim($_POST['Rayons']))) : '';
+$SMT = isset($_POST['SMT']) ? htmlspecialchars(strip_tags(trim($_POST['SMT']))) : '';
 
 
-$name =  htmlspecialchars(strip_tags(trim($_POST['name'])));
-$email =  htmlspecialchars(strip_tags(trim($_POST['email']))) ;
-$Territory = htmlspecialchars(strip_tags(trim($_POST['Territory']))) ;
-$Towns =  htmlspecialchars(strip_tags(trim($_POST['Towns']))) ;
-$RayonsTowns =  htmlspecialchars(strip_tags(trim($_POST['RayonsTowns'])));
-$Rayons = htmlspecialchars(strip_tags(trim($_POST['Rayons']))) ;
-$SMT =  htmlspecialchars(strip_tags(trim($_POST['SMT']))) ;
-
-if(($name && $Territory && $email)==''){die();}
-else {
+if (($name and $email and $Territory ) == '') {
+    die();
+} else {
     $userData = [
         'name' => $name,
         'email' => $email,
-        'territory' => $Territory . ' ' . $Rayons . ' ' . $Towns . ' ' . $RayonsTowns . ' ' . $SMT
+        'territory' => $Territory . ' ' . $Rayons . ' ' . $Towns . ' ' . $RayonsTowns . ' ' . $SMT,
     ];
     $user = new Addusers($userData);
     $result = $user->getByEmail();
     if ($email !== $result['email']) {
         $save = $user->save();
-        $response['newUser']=$userData;
+        $response['newUser'] = $userData;
     } else {
-            $response['oldUser']=$result;
-        }
+        $response['oldUser'] = $result;
     }
+}
 echo json_encode($response);
--- Artjoker/ajaxQuery.js
+++ Artjoker/ajaxQuery.js
@@ -1,18 +1,21 @@
-    (function($,undefined){
-    $(function(){
-    
-    var form = $("#myForm");
-    $("#send").text("Введите все данные.......");
-    form.change(function (){
-     var selectTerritory = $('#selectTerritory').val(),
-          selectRayons = $('#selectRayons').val(),
-          selectRayonsTowns = $('#selectRayonsTowns').val(),
-           selectTowns = $('#selectTowns').val(),
-            selectSMT = $('#selectSMT').val(),
-             inputName = $('#inputName').val(),
-             inputMail = $('#inputMail').val(),
-            formdata=$("#myForm").serialize();
+(function ($, undefined) {
+    $(function () {
         
+        var form = $("#myForm");
+        $("#send").text("Введите все данные.......");
+        form.change(function () {
+            var selectTerritory = $('#selectTerritory').val(),
+                selectRayons = $('#selectRayons').val(),
+                selectRayonsTowns = $('#selectRayonsTowns').val(),
+                selectTowns = $('#selectTowns').val(),
+                selectSMT = $('#selectSMT').val(),
+                inputName = $('#inputName').val(),
+                inputMail = $('#inputMail').val(),
+                formdata = $("#myForm").serialize();
+    
+            if(selectTerritory!='' && inputName!='' && inputMail!='')
+            {$('#send').text('Зарегестрировать');}
+            
             $.ajax({
                 type: "POST",
                 url: "registration.php",
@@ -20,9 +23,9 @@
                 data: formdata,
                 success: function (data) {
                     
-              $('#selectTerritory').val(selectTerritory);
-    
-                        if(data.towns) {
+                    $('#selectTerritory').val(selectTerritory);
+                    
+                    if (data.towns) {
                         var optionTowns = '';
                         optionTowns = '<br><select id="selectTowns" class="chosen-rtl" name="Towns" title="Выберете город"> <option value="">Выберете город</option>';
                         for (var town in data.towns) {
@@ -32,89 +35,111 @@
                         $("#detaleTowns").html(optionTowns);
                         $('#selectTowns').val(selectTowns);
                     }
-    
-                    if(data.rayons_towns) {
-                var optionRayonsTowns = '';
+                    
+                    if (data.rayons_towns) {
+                        var optionRayonsTowns = '';
                         optionRayonsTowns = '<br><select id="selectRayonsTowns" class="chosen-rtl" name="RayonsTowns" title="Выберете район Города"> <option value="">Выберете район Города</option>';
-                for (var rayon in data.rayons_towns) {
-                    optionRayonsTowns += '<option value="' + data.rayons_towns[rayon] + '">' + data.rayons_towns[rayon] + '</option>';
-                }
+                        for (var rayon in data.rayons_towns) {
+                            optionRayonsTowns += '<option value="' + data.rayons_towns[rayon] + '">' + data.rayons_towns[rayon] + '</option>';
+                        }
                         optionRayonsTowns += '</select><br>';
-                $("#detaleRayonsTowns").html(optionRayonsTowns);
-                $('#selectRayonsTowns').val(selectRayonsTowns);
-            }
+                        $("#detaleRayonsTowns").html(optionRayonsTowns);
+                        $('#selectRayonsTowns').val(selectRayonsTowns);
+                    }
                     
-               if(data.rayons) {
-                var optionRayons = '';
-                optionRayons = '<br><select id="selectRayons" class="chosen-rtl" name="Rayons" title="Выберете район Области"> <option value="">Выберете район Области</option>';
-                for (var rayon in data.rayons) {
-                    optionRayons += '<option value="' + data.rayons[rayon] + '">' + data.rayons[rayon] + '</option>';
-                }
-                optionRayons += '</select><br>';
-                $("#detaleRayons").html(optionRayons);
-                $('#selectRayons').val(selectRayons);
-            }
+                    if (data.rayons) {
+                        var optionRayons = '';
+                        optionRayons = '<br><select id="selectRayons" class="chosen-rtl" name="Rayons" title="Выберете район Области"> <option value="">Выберете район Области</option>';
+                        for (var rayon in data.rayons) {
+                            optionRayons += '<option value="' + data.rayons[rayon] + '">' + data.rayons[rayon] + '</option>';
+                        }
+                        optionRayons += '</select><br>';
+                        $("#detaleRayons").html(optionRayons);
+                        $('#selectRayons').val(selectRayons);
+                    }
                     
-           if(data.smt) {
-                var optionSMT = '';
-                optionSMT = '<br><select id="selectSMT" class="chosen-rtl" name="SMT" title="Выберете ПГТ,Село,Деревню"> <option value="">Выберете ПГТ,Село,Деревню</option>';
-                for (var smt in data.smt) {
-                    optionSMT += '<option value="' + data.smt[smt] + '">' + data.smt[smt] + '</option>'
-                }
-               optionSMT += '</select><br>';
-                $("#detaleSMT").html(optionSMT);
-                  $('#selectSMT').val(selectSMT);
-            }
-            
+                    if (data.smt) {
+                        var optionSMT = '';
+                        optionSMT = '<br><select id="selectSMT" class="chosen-rtl" name="SMT" title="Выберете ПГТ,Село,Деревню"> <option value="">Выберете ПГТ,Село,Деревню</option>';
+                        for (var smt in data.smt) {
+                            optionSMT += '<option value="' + data.smt[smt] + '">' + data.smt[smt] + '</option>'
+                        }
+                        optionSMT += '</select><br>';
+                        $("#detaleSMT").html(optionSMT);
+                        $('#selectSMT').val(selectSMT);
+                    }
+                    
                 }
             })
-    });
-  
-
-    $("#send").click(function (e){
-        e.preventDefault();
-        $('#send').text('Зарегестрировать');
-       var mydata = $("#myForm").serialize();
-        $.ajax({
-            type: "POST",
-            url: "add.php",
-            dataType: "JSON",
-            data: mydata,
-            cache:false,
-            success: function (data) {
-                $("<h1>Данные успешно отправлены</h1>").appendTo("#yes");
-                $('input, select').val('');
-                
-                if (data.newUser){
-                    var newUser='';
-                    newUser+='<h1>Ура новый пользователь</h1>'
-                    newUser+='<p>';
-                    for( var i=0 in data.newUser){
-                        newUser+=data.newUser[i]+'<br>';
-                    };
-                    newUser+='</p>';
-                    $('#yes').html(newUser);
-                }
-                
-                if(data.oldUser){
-                    var oldUser='';
-                    oldUser+='<h1>Уже есть такой пользователь</h1>'
-                    oldUser+='<p>';
-                    for( var y=0 in data.oldUser){
-                        oldUser+=data.oldUser[y]+'<br>';
-                    };
-                    oldUser+='</p>';
-                    $('#yes').html(oldUser);
-                }
-                
-            }
         });
-    });
-
+        
+        
+        $("#send").click(function (e) {
+            e.preventDefault();
+            var mydata = $("#myForm").serialize();
+            $.ajax({
+                type: "POST",
+                url: "add.php",
+                dataType: "JSON",
+                data: mydata,
+                cache: false,
+                success: function (data) {
+                    $("<h1>Данные успешно отправлены</h1>").appendTo("#yes");
+                    $('input, select').val('');
+                    
+                    if (data.newUser) {
+                        var newUser = '';
+                        newUser += '<h1>Ура новый пользователь</h1>'
+                        newUser += '<p>';
+                        for (var i = 0 in data.newUser) {
+                            newUser += data.newUser[i] + '<br>';
+                        }
+                        ;
+                        newUser += '</p>';
+                        $('#yes').html(newUser);
+                    }
+                    
+                    if (data.oldUser) {
+                        var oldUser = '';
+                        oldUser += '<h1>Уже есть такой пользователь</h1>'
+                        oldUser += '<p>';
+                        for (var y = 0 in data.oldUser) {
+                            oldUser += data.oldUser[y] + '<br>';
+                        }
+                        ;
+                        oldUser += '</p>';
+                        $('#yes').html(oldUser);
+                    }
+                    setTimeout(function() {window.location.reload();}, 5000);
+                },
+                error: function (jqXHR, exception) {
+                getErrorMessage(jqXHR, exception)}
+            });
+        });
     
+        function getErrorMessage(jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            $('#yes').html(msg);
+        }
+        
     });
     
-    })(jQuery);
+})(jQuery);
 
 
 // $('#myForm').find('input, select').each(function () {
@@ -125,41 +150,41 @@
 
 
 /*
-    $("#myForm").submit(function() {
-        var form = $(this);
-        var error = false;
-        form.find('input, select').each(function() {
-            if ($(this).val() == '') {
-                alert('Зaпoлнитe пoлe "' + $(this).attr('title') + '"!');
-                error = true;
-            }
-        })
-    });
-    
-    
-    if (!error) {
-        var data = $("#myForm").serialize();
-        $.ajax({
-            type: "POST",
-            url: "registration.php",
-            dataType: 'json',
-            data: data,
-            beforeSend: function(data) {
-                form.find('button[type="submit"]').attr('disabled', 'disabled');
-            },
-            success: function(data)
-                {
-                    console.log(info);
-                },
-            error: function(xhr, ajaxOptions, thrownError) {
-                alert(xhr.status);
-                alert(thrownError);
-            },
-            complete: function(data) {
-                form.find('button[type="submit"]').prop('disabled', false);
-            }
-        });
-        return false;
-    }
-*/
+ $("#myForm").submit(function() {
+ var form = $(this);
+ var error = false;
+ form.find('input, select').each(function() {
+ if ($(this).val() == '') {
+ alert('Зaпoлнитe пoлe "' + $(this).attr('title') + '"!');
+ error = true;
+ }
+ })
+ });
+ 
+ 
+ if (!error) {
+ var data = $("#myForm").serialize();
+ $.ajax({
+ type: "POST",
+ url: "registration.php",
+ dataType: 'json',
+ data: data,
+ beforeSend: function(data) {
+ form.find('button[type="submit"]').attr('disabled', 'disabled');
+ },
+ success: function(data)
+ {
+ console.log(info);
+ },
+ error: function(xhr, ajaxOptions, thrownError) {
+ alert(xhr.status);
+ alert(thrownError);
+ },
+ complete: function(data) {
+ form.find('button[type="submit"]').prop('disabled', false);
+ }
+ });
+ return false;
+ }
+ */
     
