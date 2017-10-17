342f1581a07353b5924a65f7174b934bc08fbd52
Dimka <manager@perilman.com.ua>
2017-10-17 23:48:59 +0300

    reg Exp worck in registration

--- ANT/registration.php
+++ ANT/registration.php
@@ -17,8 +17,8 @@ $seurname = isset($_POST['seurname']) ? mb_substr(htmlspecialchars(strip_tags(tr
 $sex = isset($_POST['sex']) ? mb_substr(htmlspecialchars(strip_tags(trim($_POST['sex']))), 0,1,'UTF-8')  : '';
 $group = isset($_POST['group']) ? mb_substr(htmlspecialchars(strip_tags(trim($_POST['group']))), 0,4,'UTF-8')  : '';
 $email = isset($_POST['email']) ? mb_substr(htmlspecialchars(strip_tags(trim($_POST['email']))), 0,40,'UTF-8')  : '';
-$balls = isset($_POST['balls']) ?  mb_substr(htmlspecialchars(strip_tags(trim($_POST['balls']))), 0,2,'UTF-8') : '';
-$berd_year = isset($_POST['berd_year']) ? mb_substr(htmlspecialchars(strip_tags(trim($_POST['berd_year']))), 0,3,'UTF-8') : '';
+$balls = isset($_POST['balls']) ?  mb_substr(htmlspecialchars(strip_tags(trim($_POST['balls']))), 0,4,'UTF-8') : '';
+$berd_year = isset($_POST['berd_year']) ? mb_substr(htmlspecialchars(strip_tags(trim($_POST['berd_year']))), 0,4,'UTF-8') : '';
 $place = isset($_POST['place']) ? mb_substr(htmlspecialchars(strip_tags(trim($_POST['place']))), 0,20,'UTF-8')  : '';
 
 $name=(string)$name;
@@ -30,7 +30,7 @@ $balls=(int)$balls;
 $berd_year=(string)$berd_year;
 $place=(string)$place;
 
-if(preg_match('/^[\wа-яёії]{3,20}$/i', $name)===1){
+if(preg_match('/\D/i', $name)===1){
     $userData['name']=$name;
 }else{
     $userData['name']='ОШИБКА';
@@ -38,7 +38,7 @@ if(preg_match('/^[\wа-яёії]{3,20}$/i', $name)===1){
 }
 
 
-if(preg_match('/^[\wа-яёії]{3,20}$/i', $seurname)===1){
+if(preg_match('/\D/i', $seurname)===1){
     $userData['seurname']=$seurname;
 }else{
     $userData['seurname']='ОШИБКА';
@@ -70,7 +70,7 @@ if(preg_match('/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/i', $email)===
 }
 
 
-if(preg_match('/^[0-9]{1,3}$/', $balls)===1){
+if(preg_match('/^[0-9]{1,4}$/', $balls)===1){
     $userData['balls']=$balls;
 }else{
     $userData['balls']='ОШИБКА';
@@ -86,7 +86,7 @@ if(preg_match('/^[0-9]{4}$/', $berd_year)===1){
 }
 
 
-if(preg_match('/^[\wа-яёії]{3,20}$/i', $place)===1){
+if(preg_match('/\D/i', $place)===1){
     $userData['place']=$place;
 }else{
     $userData['place']='ОШИБКА';
@@ -95,7 +95,9 @@ if(preg_match('/^[\wа-яёії]{3,20}$/i', $place)===1){
 
 
 if (in_array('ОШИБКА', $userData,true)){
-echo json_encode($userData);
+echo "<pre>";
+    var_dump( $userData );
+echo "</pre>";
 }else{
     (new Addusers($userData))->save();
 };
@@ -104,26 +106,26 @@ echo json_encode($userData);
 
 
 
-//function great_file($name,$seurname,$sex,$group,$email,$balls,$berd_year,$place)
-//{
-//    $date = date("Y-M-d H:i:s");
-//    $handle = 'REGISTRATION.txt';
-//    $string = $date . ' name :' . ' > [ ' . $name . ' ]' . PHP_EOL;
-//    $string .= $date . ' seurname :' . ' > [ ' . $seurname . ' ]' . PHP_EOL;
-//    $string .= $date . ' sex :' . ' > [ ' . $sex . ' ]' . PHP_EOL;
-//    $string .= $date . ' group :' . ' > [ ' .$group  . ' ]' . PHP_EOL;
-//    $string .= $date . ' email :' . ' > [ ' . $email . ' ]' . PHP_EOL;
-//    $string .= $date . ' balls :' . ' > [ ' . $balls . ' ]' . PHP_EOL;
-//    $string .= $date . ' berd_year :' . ' > [ ' . $berd_year . ' ]' . PHP_EOL;
-//    $string .= $date . ' place :' . ' > [ ' . $place. ' ]' . PHP_EOL;
-//    $string.=PHP_EOL;
-//    $text = fopen($handle, 'w+');
-//    fwrite($text, $string);
-//    fclose($text);
-//    return;
-//}
-//
-//great_file($name, $seurname, $sex, $group, $email, $balls, $berd_year, $place);
+function great_file($name,$seurname,$sex,$group,$email,$balls,$berd_year,$place)
+{
+    $date = date("Y-M-d H:i:s");
+    $handle = 'REGISTRATION.txt';
+    $string = $date . ' name :' . ' > [ ' . $name . ' ]' . PHP_EOL;
+    $string .= $date . ' seurname :' . ' > [ ' . $seurname . ' ]' . PHP_EOL;
+    $string .= $date . ' sex :' . ' > [ ' . $sex . ' ]' . PHP_EOL;
+    $string .= $date . ' group :' . ' > [ ' .$group  . ' ]' . PHP_EOL;
+    $string .= $date . ' email :' . ' > [ ' . $email . ' ]' . PHP_EOL;
+    $string .= $date . ' balls :' . ' > [ ' . $balls . ' ]' . PHP_EOL;
+    $string .= $date . ' berd_year :' . ' > [ ' . $berd_year . ' ]' . PHP_EOL;
+    $string .= $date . ' place :' . ' > [ ' . $place. ' ]' . PHP_EOL;
+    $string.=PHP_EOL;
+    $text = fopen($handle, 'w+');
+    fwrite($text, $string);
+    fclose($text);
+    return;
+}
+
+great_file($name, $seurname, $sex, $group, $email, $balls, $berd_year, $place);
 
  
 
