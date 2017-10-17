bef97ad5fbb8bb1964f4550603d25162428660fd
Dimka <manager@perilman.com.ua>
2017-10-18 02:55:52 +0300

    new calss Scroll

---
+++ ANT/CLASSES/Scroll.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * ${CLASS_NAME}
+ *
+ * @category  Cgi
+ * @package   ${PARAM_DOC}
+ * @author    CGI <info.de@cgi.com>
+ * @copyright 2016 CGI
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.de.cgi.com/
+ */
+class Scroll extends Entyty
+{
+   public function show(){
+       $db=DB::getInstance();
+       $query="";
+   }
+}
