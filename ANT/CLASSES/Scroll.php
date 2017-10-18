300a285d2e54add2ed4874c2c6ae8824d981c434
Dimka <manager@perilman.com.ua>
2017-10-18 03:23:23 +0300

    class do

--- ANT/CLASSES/Scroll.php
+++ ANT/CLASSES/Scroll.php
@@ -1,19 +1,16 @@
 <?php
 
-/**
- * ${CLASS_NAME}
- *
- * @category  Cgi
- * @package   ${PARAM_DOC}
- * @author    CGI <info.de@cgi.com>
- * @copyright 2016 CGI
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @link      http://www.de.cgi.com/
- */
 class Scroll extends Entyty
 {
    public function show(){
        $db=DB::getInstance();
-       $query="";
+       $query="SELECT `name`, `seurname`,`group`,`balls` FROM users ORDER BY balls DESC";
+       $result=$db->query($query);
+       if (!$result) {die($db->error);}
+       $json=[];
+       while ($row=$result->fetch_assoc()){
+           $json[]=$row;
+       }
+      return $json;
    }
 }
