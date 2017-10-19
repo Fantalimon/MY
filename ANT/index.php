d1f32c1b1a20437802f727f0a403c7318005a266
Dimka <manager@perilman.com.ua>
2017-10-19 19:22:31 +0300

    add favicon

--- ANT/index.php
+++ ANT/index.php
@@ -10,7 +10,7 @@ include_once 'autoload.php';
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Список</title>
     
 <link href="css/bootstrap.css" rel="stylesheet">
